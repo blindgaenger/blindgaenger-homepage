1d3d4b643faaf717b91ca1d565bf1c350f4271de
blindgaenger <blindgaenger@gmail.com>
2011-02-12 17:41:26 +0100

    initial commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'haml'
+require 'sass'
+
+configure do
+  set :haml, :format => :html5 # default Haml format is :xhtml
+  set :sass, :style => :compact # default Sass style is :nested, :expanded
+end
+
+get '/' do
+  haml :index
+end
+
+get '/stylesheets/:file.css' do
+   sass params[:file].to_sym 
+end
