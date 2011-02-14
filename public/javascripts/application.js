c0859d7c68fcd6fb7a009ab51c586d83e11ce579
blindgaenger <blindgaenger@gmail.com>
2011-02-14 23:41:50 +0100

    added an icon per page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,35 @@
-$(function(){
+jQuery.fn.anchorScroll = function(callback) {
+  $.anchorIds = {};
 
-	$("nav a").anchorAnimate();
+  $(this).each(function(){
+    $.anchorIds[$(this).attr('id')] = true;
+  });
+  
+  $(window).scroll(function(){
+    var scrollOffset = $(this).scrollTop();
+    for (anchorId in $.anchorIds) {
+      var anchor = $('a[id="' + anchorId + '"]');
+      if (scrollOffset >= anchor.offset().top - 700) {
+        if (scrollOffset <= anchor.offset().top + anchor.outerHeight(true)) {
+          callback(anchorId);
+          return;
+        }
+      }
+    }
+  });
+  
+};
 
+$(document).ready(function() {
+  
+  $("nav a").anchorAnimate();
+  
+  $("a[id]").anchorScroll(function(anchorId) {
+    $('nav a').removeClass('current')
+    $('nav a[href="#' + anchorId + '"]').addClass('current')
+    
+    $('#icon img').removeClass('current')
+    $('#icon img[alt="' + anchorId + '"]').addClass('current')
+  });
+  
 });
