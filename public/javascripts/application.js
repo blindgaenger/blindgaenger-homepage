760db52d802cb46c86222744a718205307633f91
blindgaenger <blindgaenger@gmail.com>
2011-02-20 14:30:54 +0100

    init the current section on page load

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,10 +2,15 @@ $(document).ready(function() {
   
   $("nav a").anchorAnimate({speed : 700});
   
-  $("h1 a[id]").anchorScroll(function() {
-    var anchorId = this.id;
+  var updateScrollObserver = function() {
+    var anchorId = $(this).attr('id');
     $('nav a').blur().selectCurrent('[href=#' + anchorId + ']');
     $('#icon img').selectCurrent('[alt=' + anchorId + ']');
-  });
+  }
   
+  var sectionAnchors = $("#content [id]")
+  var currentAnchor = $(window.location.hash);
+  if (currentAnchor.length == 0) currentAnchor = sectionAnchors.first();
+  currentAnchor.each(updateScrollObserver);
+  sectionAnchors.anchorScroll(updateScrollObserver);
 });
