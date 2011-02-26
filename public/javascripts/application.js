d13610ed694ccc7142e7e264df36550fc070d0d2
blindgaenger <blindgaenger@gmail.com>
2011-02-26 20:40:47 +0100

    use a jquery helper for the raphael icons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,26 +1,22 @@
 $(document).ready(function() {
 
-  var drawIcon = function(options) {
-    $.paper.clear();
-    var icon = icons[options['icon']];
-    var color = options['color'];
-    $.paper.path(icon).attr({fill: "315-" + color + ":30-#bbb", stroke: "#fff", "stroke-width": 2}).scale(6, 6).translate(75, 75);
-  }
-  $.paper = Raphael('paper', 180, 180);
-  $.iconMap = {
-    start:     {icon: 'home',    color: '#fff'},
-    blog:      {icon: 'feed',    color: '#fff'},
-    twitter:   {icon: 'twitter', color: '#fff'},
-    posterous: {icon: 'cloudUp', color: '#fff'},
-    facebook:  {icon: 'user',    color: '#fff'},
-    github:    {icon: 'github',  color: '#fff'}
+  var iconMap = {
+    start:     'home',
+    blog:      'feed',
+    twitter:   'twitter',
+    posterous: 'cloudUp',
+    facebook:  'user',
+    github:    'github'
   }
   
   $("nav a[href^='#']").anchorAnimate({speed : 700});
   $("#content section[id]").viewport(function() {
     var anchorId = $(this).attr('id');
     $('nav a').blur().selectCurrent('[href=#' + anchorId + ']');
-    drawIcon($.iconMap[anchorId])
+    
+    var icon = iconMap[anchorId]
+    var attr = {fill: "315-#fff:30-#bbb", stroke: "#fff", "stroke-width": 2}
+    $('#paper').drawIcon(icon, attr)
   });
-
+  
 });
