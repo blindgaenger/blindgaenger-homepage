043de4369b263960b6d81607f14acac3d894b1c2
blindgaenger <blindgaenger@gmail.com>
2011-02-27 14:20:14 +0100

    added twitter timeline

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ configure do
 end
 
 get '/' do
+  @tweets = []
   load_tweets
   haml :index
 end
@@ -25,20 +26,35 @@ get '/stylesheets/:file.css' do
 end
 
 helpers do
-  Tweet = Struct.new(:id, :time, :text, :user, :icon)
+  Tweet = Struct.new(:id, :time, :user, :icon, :text, :html)
   def load_tweets
-    @tweets = Twitter.user_timeline('blindgaenger', :count => 10, :include_rts => true, :include_entities => true).map do |t|
+    @tweets = Twitter.user_timeline('blindgaenger', :count => 10, :include_rts => true).map do |t|
       tweet = Tweet.new(t.id, Time.parse(t.created_at))
       status = t.retweeted_status ? t.retweeted_status : t
-      tweet.text = replace_entities(status.text, status.entities)
       tweet.user = status.user.screen_name
       tweet.icon = status.user.profile_image_url
+      tweet.text = status.text
+      tweet.html = replace_entities(status.text, status.entities)
       tweet
     end
   end
   
   def replace_entities(text, entities)
-    text #TODO
+    html = text.dup
+
+    html = html.gsub(/\b(([\w-]+:\/\/?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/)))/) do |link|
+      schema = $2.match(/w/) ? "http://" : ""
+      text = (link.length > 25) ? link[0..24] + "..." : link
+      '<a class="link" href="' + schema+link + '" target="_blank">' + text + '</a>'
+    end
+    
+    html.gsub!(/\B#([a-zA-Z0-9_]{1,20})([\s,]|$)/,
+      '<a class="hash" href="http://twitter.com/search?q=%23\1" target="_blank">#\1</a>\2')
+    html
+    
+    html.gsub!(/\B@([a-zA-Z0-9_]{1,20})([\s,]|$)/,
+      '@<a class="mention" href="http://twitter.com/\1" target="_blank">\1</a>\2')
+    html
   end
   
 end
