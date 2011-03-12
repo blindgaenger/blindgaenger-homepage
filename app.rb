2eb36426b75ef41043218796ea33405c98cf5596
blindgaenger <blindgaenger@gmail.com>
2011-03-12 19:40:44 +0100

    removed unused webfont mime types

--- app.rb
+++ app.rb
@@ -10,11 +10,6 @@ require File.expand_path(File.join('config', 'environment'), File.dirname(__FILE
 configure do
   set :haml, :format => :html5 # default Haml format is :xhtml
   set :sass, :style => :compact # default Sass style is :nested, :expanded
-
-  [:eot, :woff, :ttf, :otf ].each do |ext|
-    mime_type ext, 'application/octet-stream'
-  end
-  mime_type :svg, 'image/svg+xml'
 end
 
 helpers do
