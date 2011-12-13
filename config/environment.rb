3678a176c094f3979cf11ac604ce852ec1143346
blindgaenger <blindgaenger@gmail.com>
2011-12-14 00:48:31 +0100

    set min_messages for downgraded version

--- config/environment.rb
+++ config/environment.rb
@@ -8,12 +8,13 @@ db = URI.parse(ENV['DATABASE_URL'] || 'postgres://bjuenger:@localhost/homepage_b
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.logger.level = Logger::WARN
 ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
+  :adapter      => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host         => db.host,
+  :username     => db.user,
+  :password     => db.password,
+  :database     => db.path[1..-1],
+  :encoding     => 'utf8',
+  :min_messages => 'notice'
 )
 
 $LOAD_PATH.unshift File.expand_path(File.join('..', 'models'), File.dirname(__FILE__))
