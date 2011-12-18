e9fc17839d63025533fd358ca203cbc48c28f352
blindgaenger <blindgaenger@gmail.com>
2011-12-18 22:36:12 +0100

    use gimmebar to fetch tumbles

--- models/tumble.rb
+++ models/tumble.rb
@@ -2,55 +2,48 @@ require 'open-uri'
 require 'crack'
 
 class Tumble < ActiveRecord::Base
-  URL = "http://posterous.com/api/2/users/60745/sites/blindgaenger/posts/public"
+  URL = "https://gimmebar.com/api/v0/public/assets/blindgaenger/firehose"
   BUNCH_SIZE = 9
 
   scope :history, :order => "posted_at DESC"
-  
+
   class << self
     def fetch!
-      url = URL
-      if latest_id = self.latest.try(:post_id)
-        url += "?since_id=#{latest_id}"
-      end
-      
+      url = "#{URL}?limit=#{BUNCH_SIZE}"
+
       json = open(url).read
-      posts = Crack::JSON.parse(json)
+      posts = Crack::JSON.parse(json)['records']
       posts.each do |post|
         begin
-          html = post['body_full']
-          html.gsub!('\u003C', '<')
-          html.gsub!('\u003E', '>')
-          html.gsub!('width="500"', 'width="250"')
-          html.gsub!('height="412"', 'height="206"')
-          
-          self.create(
+          model = self.find_by_post_id(post['id']) || self.new
+          model.attributes = {
             :post_id => post['id'],
-            :url => post['full_url'],
+            :url => post['source'],
             :title => post['title'],
-            :body => html,
-            :posted_at => Time.parse(post['display_date'])
-          )
+            :body => post['content'].to_yaml,
+            :posted_at => Time.at(post['date'])
+          }
+          model.save
         rescue => ex
           warn "could not store tumble: #{ex.message}"
         end
       end
-      
+
       self.history.all(:offset => BUNCH_SIZE*2).each(&:delete)
-      
+
       posts
     end
-    
+
     def bunch
       self.history.all(:limit => BUNCH_SIZE)
     end
-    
+
     def latest
       self.history.first
     end
   end
-  
+
   def text
   end
-  
+
 end
